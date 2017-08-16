887450f649c810631e5a53c808d518765fc35c98
mlowe12 <mlowe12@ufl.edu>
2017-08-15 23:57:18 -0400

    Initial Commit

---
+++ NaiveBayesFilter/src/java/WordMap.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*; 
+import java.lang.*;
+import org.apache.hadoop.io.*; 
+import org.apache.hadoop.conf.*; 
+import org.apache.hadoop.mapreduce.*;  
+
+
+public class WordMap{
+
+    public static class Map extends Mapper<LongWritable, Text, Text, IntWritable>{
+
+        public void map(LongWritable key, Text value, Context context) throws InterruptedException, IOException{
+            String observedLined = value.toString(); 
+            StringTokenizer tok = new StringTokenizer(observedLine); 
+            while(tok.hasMoreTokens()){
+                value.set(tok.nextToken()); 
+                context.write(value, new IntWritable());
+
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        System.out.println("Success");
+    }
+}
